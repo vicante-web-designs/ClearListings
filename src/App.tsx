ca90322443e7b213544adcc6911ee85039ae0b70
Batubo Victory <victorybatubo76@gmail.com>
2026-02-03 17:59:07 +0100

    feat: Create page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import HomePage from './pages/HomePage'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} /> 
+      </Routes>
+    </Router>
   )
 }
 
