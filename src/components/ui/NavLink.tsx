3e44d9d08a8fa3e91548bdada03315e284e58fd4
Batubo Victory <victorybatubo76@gmail.com>
2026-02-04 17:31:53 +0100

    style: Add styling to navLink component

--- src/components/ui/NavLink.tsx
+++ src/components/ui/NavLink.tsx
@@ -1,8 +1,8 @@
 import type { NavLinkProps } from '../../types/UiTypes';
 
-function NavLink ({link, label, type}: NavLinkProps){
+function NavLink ({link, label}: NavLinkProps){
     return(
-        <a href={link} className={`${type}`}>
+        <a href={link} className={`border-b-2 border-b-blue-100 hover:border-b-2 px-2 transition-all hover:border-blue-500 ease-in duration-300`}>
             {label}
         </a>
     )
