c3a96e5d909327d127f855ac003566088d7866bc
Batubo Victory <victorybatubo76@gmail.com>
2026-02-04 17:45:41 +0100

    style: Share some styles between all buttons

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,14 +4,14 @@ import type { ButtonVariant } from '../../types/UiTypes';
 function Button({label, type, variant}: ButtonProps){
 
     const variants: ButtonVariant = {
-        primary: 'px-6 py-3 bg-blue-600 text-white rounded-full hover:bg-blue-400 transition-all ease-in duration-200 shadow-2xl shadow-blue-400',
+        primary: 'bg-blue-600 hover:bg-blue-400 shadow-blue-400',
         secondary: '',
     }
 
     return(
         <button
             type={type}
-            className={`${variants[variant]}`}
+            className={`px-6 py-3 text-white rounded-full transition-all ease-in duration-200 shadow-2xl ${variants[variant]}`}
         >
             {label}
         </button>
