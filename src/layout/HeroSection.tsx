534a2ecd676b19a7d23a3e3b7b7c3bf5bf54716a
Batubo Victory <victorybatubo76@gmail.com>
2026-02-05 17:53:24 +0100

    feat: Finish Hero Section content

--- src/layout/HeroSection.tsx
+++ src/layout/HeroSection.tsx
@@ -1,6 +1,6 @@
 function HeroSection(){
     return(
-        <section className='h-screen'>
+        <section className='h-screen relative'>
             <div className='relative w-full h-screen'>
                 <img
                     sizes="(max-width: 1084px) 100vw, 1084px"
@@ -15,9 +15,21 @@ function HeroSection(){
                     alt="Hero Section Image"
                     className='absolute -z-1 w-full h-full'></img>
                 <div
-                    className='w-full h-full bg-[rgba(0,0,0,0.3)]'
+                    className='w-full h-full bg-[rgba(0,0,0,0.7)]'
                 ></div>
             </div>
+
+            <article className='absolute z-2 *:text-white *:text-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/12 w-full h-fit flex flex-col gap-8 items-center'>
+                <h1 className='text-[96px] leading-20 tracking-tighter font-bold'>
+                    Find Your Next Home, <span className='text-orange-500'>
+                        Faster.
+                    </span>
+                </h1>
+
+                <h2 className='text-2xl font-light leading-[1.6em] max-w-[80%]'>
+                    Browse verified listings in real estate and architecture, designed for modern buyers and sellers.
+                </h2>
+            </article>
         </section>
     )
 }
