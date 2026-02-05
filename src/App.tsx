f84c6350d0c662b90fde843eba9b07b570191184
Batubo Victory <victorybatubo76@gmail.com>
2026-02-05 18:13:16 +0100

    feat: Create new route for CreateListingsPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import CreateListingPage from './pages/CreateListingPage'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/createListings' element={<CreateListingPage />} />
       </Routes>
     </Router>
   )
