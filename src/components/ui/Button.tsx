23afa8a8b9713a661d01481c551f69420790a50e
Batubo Victory <victorybatubo76@gmail.com>
2026-02-05 18:12:50 +0100

    feat: Add onClick prop

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,7 @@
 import type { ButtonProps } from '../../types/UiTypes';
 import type { ButtonVariant } from '../../types/UiTypes';
 
-function Button({label, type, variant}: ButtonProps){
+function Button({label, type, variant, onClick}: ButtonProps){
 
     const variants: ButtonVariant = {
         primary: 'bg-blue-600 hover:bg-blue-400 shadow-blue-400 shadow-2xl text-white',
@@ -11,6 +11,7 @@ function Button({label, type, variant}: ButtonProps){
 
     return(
         <button
+            onClick={onClick}
             type={type}
             className={`px-6 py-3 rounded-full transition-all ease-in duration-200 ${variants[variant]}`}
         >
