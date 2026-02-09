b44a8d429e9d5172d592f8bb65674af88b122226
Batubo Victory <victorybatubo76@gmail.com>
2026-02-09 10:00:49 +0100

    feat: Use formData Api

--- src/components/ui/FormField.tsx
+++ src/components/ui/FormField.tsx
@@ -1,6 +1,6 @@
-import type { FormInputProps } from '../../types/UiTypes';
+import type { FormFieldProps } from '../../types/UiTypes';
 
-function FormField({ id, type, label, placeholder }: FormInputProps){
+function FormField({ id, type, label, placeholder, value, onChange }: FormFieldProps){
     return(
         <div className='flex flex-col w-full gap-2'>
             <label htmlFor={id} className='font-bold'>
@@ -8,11 +8,14 @@ function FormField({ id, type, label, placeholder }: FormInputProps){
             </label>
 
             <input
+                name={id.toLowerCase()}
                 type={type}
                 id={id}
                 title={id}
                 placeholder={placeholder}
                 className='bg-gray-200 rounded-full py-3 px-6 focus:bg-blue-200 outline-0 caret-blue-800'
+                value={value}
+                onChange={onChange}
             />
         </div>
     )
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,29 @@
 import Navbar from '../layout/Navbar'
 import HeroSection from '../layout/HeroSection'
+import { useContext } from 'react'
+import { ListingContext } from '../context/ListingContext/createListingContext'
 
 function HomePage(){
+    const context = useContext(ListingContext)
+
+    const { listings } = context;
     return(
         <main className='flex flex-col gap-40'>
             <Navbar />
             <HeroSection />
+
+            <section className='flex w-full justify-between'>
+                {
+                    listings && listings.map(listing => (
+                        <article>
+                            <p>{listing.title}</p>
+                            <p>{listing.description}</p>
+                            <p>{listing.location}</p>
+                            <p>{listing.price}</p>
+                        </article>
+                    ))
+                }
+            </section>
         </main>
     )
 }
