98bd5090ff41e6634fcb5d2e68c36a0b5feb1228
Batubo Victory <victorybatubo76@gmail.com>
2026-02-26 13:06:48 +0100

    feat: Create dynamic route for listings

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,18 @@ import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage.tsx';
 import { ListingProvider } from './context/ListingContext/context.tsx';
+import ListingDetails from './pages/listings/ListingDetails.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />
   },
+
+  {
+    path:'/listings/:id',
+    element: <ListingDetails />
+  }
 ])
 
 createRoot(document.getElementById('root')!).render(
