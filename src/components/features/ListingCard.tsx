d8d7cbfc140c072b72b3a29451c833f5d68a63d4
Batubo Victory <victorybatubo76@gmail.com>
2026-02-26 13:30:33 +0100

    feat: Implement dynamic routing for listings

--- src/components/features/ListingCard.tsx
+++ src/components/features/ListingCard.tsx
@@ -1,61 +1,65 @@
+import { Link } from 'react-router-dom'
 import type { ListingCardProps } from '../../types/Listing'
 import { formatPrice } from '../../utils/formatPrice'
 import Button from '../ui/Button'
 
 function ListingCard({id, images, title, location, city, state, price, description }: ListingCardProps){
     return(
-        <article
-            id={id}
-            className='bg-neutral-100 w-100 shadow-lg rounded-2xl overflow-hidden flex flex-col h-fit'
-        >
-            {
-                images && (
-                    <div className='overflow-hidden'>
-                        <img
-                            src={images[0]}
-                            className='w-full hover:scale-110 transition-all ease-in-out duration-500'
-                            alt="" 
+        
+            <article
+                id={id}
+                className='bg-neutral-100 w-100 shadow-lg rounded-2xl overflow-hidden flex flex-col h-fit'
+            >
+                {
+                    images && (
+                        <div className='overflow-hidden'>
+                            <img
+                                src={images[0]}
+                                className='w-full hover:scale-110 transition-all ease-in-out duration-500'
+                                alt="" 
+                            />
+                        </div>
+                    )
+                }
+
+                <article className='px-16 py-24 flex flex-col gap-6'>
+
+                    <div>
+                        <h3>
+                            {title}
+                        </h3>
+                        
+                        { // Listing Price 
+                            price && (
+                                <p className='text-xl font-bold text-blue-800'>
+                                    {formatPrice(price)}
+                                </p>
+                            )
+                        }
+
+                        <p>
+                            {description}
+                        </p>
+                    </div>
+
+                    <p className='font-semibold text-neutral-500'>
+                        {`${location}, ${city}, ${state}`}
+                    </p>
+
+                </article>
+
+                <Link to={`/listings/${id}`}>
+                    <div className='flex justify-center *:w-full px-16 pb-16'>
+                        <Button 
+                            label='View details'
+                            type='button'
+                            variant='secondary'
                         />
                     </div>
-                )
-            }
+                </Link>
 
-            <article className='px-16 py-24 flex flex-col gap-6'>
-
-                <div>
-                    <h3>
-                        {title}
-                    </h3>
-                    
-                    { // Listing Price 
-                        price && (
-                            <p className='text-xl font-bold text-blue-800'>
-                                {formatPrice(price)}
-                            </p>
-                        )
-                    }
-
-                    <p>
-                        {description}
-                    </p>
-                </div>
-
-                <p className='font-semibold text-neutral-500'>
-                    {`${location}, ${city}, ${state}`}
-                </p>
 
             </article>
-
-            <div className='flex justify-center *:w-full px-16 pb-16'>
-                <Button 
-                    label='View details'
-                    type='button'
-                    variant='secondary'
-                />
-            </div>
-
-
-        </article>
     )
 }
 
--- src/pages/listings/ListingDetails.tsx
+++ src/pages/listings/ListingDetails.tsx
@@ -1,11 +1,24 @@
 import { useParams } from 'react-router-dom'
+import { useContext } from 'react';
+import { ListingContext } from '../../context/ListingContext/createListingContext';
+import type { Listing } from '../../types/Listing';
 
 const ListingDetails = () => {
-    const params = useParams();
-    console.log(params)
+    const { listingId } = useParams();
+
+    const context = useContext(ListingContext)
+
+    if(!context){
+        throw new Error('Context not found')
+    }
+
+    const { listings } = context;
+
+    const listing = listings.find((listing: Listing) => listing.id === listingId)
+
   return (
     <div>
-      
+        {listing ? listing.title : ''}
     </div>
   )
 }
