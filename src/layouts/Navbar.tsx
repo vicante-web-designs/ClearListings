825907eaf18ac1a91a77f488adc07a0c090fb314
Batubo Victory <victorybatubo76@gmail.com>
2026-02-26 14:34:32 +0100

    style: Make navbar fixed on scroll

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,41 +1,44 @@
 import Logo from '../../public/Images/Logo/ClearListing_Logo.png'
 import NavLink from '../components/ui/NavLink'
+import { useState, useEffect } from 'react'
 
 const Navbar = () => {
+    const [isSticky, setIsSticky] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsSticky(window.scrollY > 80)
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
 
     return(
-        <nav className='rounded-full h-20 px-24
-         py-16 shadow-2xl bg-blue-100 flex items-center justify-between mx-8 my-10 fixed z-999 left-0 right-0'>
+        <>
+            {/* placeholder to prevent content jump when navbar becomes fixed */}
+            <div className='h-36'/>
 
-            <img src={Logo} alt="ClearListing Logo" className='h-full rounded-full'/>
+            <nav className={`rounded-full h-20 px-24 py-16 flex items-center justify-between mx-8 z-50 left-0 right-0 fixed transition-all duration-500 ease-in-out ${
+                isSticky
+                ? 'top-4 bg-white shadow-2xl'
+                : 'top-10 bg-blue-100 shadow-2xl'
+            }`}>
 
-            <div className='md:flex w-full justify-between hidden'>
-                <NavLink 
-                    label='Home'
-                    link='#'
-                />
-                <NavLink 
-                    label='Listings'
-                    link='#'
-                />
-                <NavLink 
-                    label='About'
-                    link='#'
-                />
-                <NavLink 
-                    label='Contact'
-                    link='#'
-                />
-                <NavLink 
-                    label='Admin'
-                    link='#'
-                />
-            </div>
+                <img src={Logo} alt="ClearListing Logo" className='h-full rounded-full'/>
 
-            <div className="md:hidden">
-                ☰
-            </div>
-        </nav>
+                <div className='md:flex w-full justify-between hidden'>
+                    <NavLink label='Home' link='#' />
+                    <NavLink label='Listings' link='#' />
+                    <NavLink label='About' link='#' />
+                    <NavLink label='Contact' link='#' />
+                    <NavLink label='Admin' link='#' />
+                </div>
+
+                <div className="md:hidden">☰</div>
+            </nav>
+        </>
     )
 }
 
