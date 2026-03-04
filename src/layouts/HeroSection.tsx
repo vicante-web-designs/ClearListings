aff24e86bc541de6767b4c45f81f006a04e9257e
Batubo Victory <victorybatubo76@gmail.com>
2026-03-04 18:28:34 +0100

    style: Align hero section content with design

--- src/layouts/HeroSection.tsx
+++ src/layouts/HeroSection.tsx
@@ -13,20 +13,20 @@ const HeroSection = () => {
                     /Images/Hero/Hero_Image/Hero_Image_w_1084.jpg 1084w"
                     src="/Images/Hero/Hero_Image/Hero_Image_w_1084.jpg"
                     alt="Hero Section Image"
-                    className='absolute -z-1 w-full h-full'></img>
+                    className='absolute -z-1 w-full h-full object-cover'></img>
                 <div
                     className='w-full h-full bg-[rgba(0,0,0,0.7)]'
                 ></div>
             </div>
 
-            <article className='absolute z-2 *:text-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/12 w-full h-fit flex flex-col gap-8 items-center'>
-                <h1 className='text-white'>
-                    Find Your Next Home, <span className='text-primary'>
+            <article className='absolute z-2 *:text-left bottom-1/16 left-0 -translate-y-1/12 w-[80%] h-fit flex flex-col gap-4 items-start pl-16'>
+                <h1 className='display-heading text-white'>
+                    Find Your Next Home, <span className='text-error font-medium'>
                         Faster.
                     </span>
                 </h1>
 
-                <h2 className='text-neutral-300'>
+                <h2 className='text-neutral-300 sub-heading'>
                     Browse verified listings in real estate and architecture, designed for modern buyers and sellers.
                 </h2>
             </article>
