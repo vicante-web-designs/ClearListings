1e049430c6a26da3492de54ea05eb954ece08234
Batubo Victory <victorybatubo76@gmail.com>
2026-03-07 13:19:04 +0100

    feat: Add listing to redux store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import listingReducer from './listings/listingsSlice';
 export const store = configureStore({
     reducer: {
-        
+        listings: listingReducer
     }
 });
 
