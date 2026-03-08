0b4fd984bdeccb203c44932176992290c557cde7
Batubo Victory <victorybatubo76@gmail.com>
2026-03-08 12:59:34 +0100

    feat: Add reducer for adding listings

--- src/state/listings/listingsSlice.ts
+++ src/state/listings/listingsSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit'
 import type { Listing } from '../../types/Listing';
 import { mockListings } from '../../data/ListingData';
 
@@ -14,7 +14,9 @@ const listingSlice = createSlice({
     name: 'listing',
     initialState,
     reducers: {
-
+        addListing: (state, action: PayloadAction<Listing>) => {
+            state.listingValue.push(action.payload)
+        }
     }
 })
 
