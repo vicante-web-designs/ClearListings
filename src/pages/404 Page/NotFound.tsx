ef9f9d2459dd3e89fb2ed80cd13846c966ef8290
Batubo Victory <victorybatubo76@gmail.com>
2026-03-09 08:24:26 +0100

    fix: Update Button component path

--- src/pages/404 Page/NotFound.tsx
+++ src/pages/404 Page/NotFound.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import Button from '../../components/ui/Button'
+import Button from '../../components/ui/Buttons/Button'
 
 const NotFound = () => {
   return (
