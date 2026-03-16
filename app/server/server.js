e1404da6b488a74c2cb45c3fe0d718b02287536f
Batubo Victory <victorybatubo76@gmail.com>
2026-03-16 12:12:02 +0100

    test: Make sure server is running perfectly

---
+++ app/server/server.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
+import express from 'express';
+import cors from 'cors';
+
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(express.json())
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.get('/api/test', (req, res) => {
+    res.json({message: "It's working"})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}`)
+})
