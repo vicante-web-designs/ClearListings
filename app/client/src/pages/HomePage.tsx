7335d7e1e3b55e7cf9504666fd32c282e4371d03
Batubo Victory <victorybatubo76@gmail.com>
2026-03-18 12:45:36 +0100

    refactor: Remove backend connection test message

--- app/client/src/pages/HomePage.tsx
+++ app/client/src/pages/HomePage.tsx
@@ -7,16 +7,9 @@ import PageLink from '../components/ui/links/PageLink'
 import { useSelector } from 'react-redux'
 import { selectFilteredListings } from '@/state/listings/listingsSlice';
 import { Button } from '@/components/ui/Buttons/button'
-import { useEffect, useState } from 'react'
 
 const HomePage = () => {
-    const [message, setMessage] = useState('');
 
-    useEffect(() => {
-        fetch(`${import.meta.env.VITE_API_URL}/api/test`)
-        .then(res => res.json())
-        .then(data => setMessage(data.message))
-    }, [])
 
     const filteredListings = useSelector(selectFilteredListings)
 
@@ -26,10 +19,6 @@ const HomePage = () => {
             <HeroSection />
 
             <SearchPanel/>
-
-            <div className='bg-black w-full h-20'>
-                <h1>{message}</h1>
-            </div>
 
             <section className='flex flex-col gap-48 items-center'>
                 <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 w-full max-w-7xl mx-auto px-6 place-items-center py-16'>
