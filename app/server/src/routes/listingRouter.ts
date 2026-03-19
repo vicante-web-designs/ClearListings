f0bd0d91a20b4e5e27798a9ef10ef9af2d53f4bb
Batubo Victory <victorybatubo76@gmail.com>
2026-03-19 10:42:19 +0100

    Add comments

--- app/server/src/routes/listingRouter.ts
+++ app/server/src/routes/listingRouter.ts
@@ -3,11 +3,13 @@ import { createListing, getAllListings } from '../services/listingService.ts';
 
 const router = express.Router()
 
+// Create listings
 router.post('/', async (req, res) => {
     const listing = await createListing(req.body);
     res.json(listing)
 })
 
+// Get all Listings
 router.get('/', async (req, res) => {
     const listings = await getAllListings()
     res.json(listings)
