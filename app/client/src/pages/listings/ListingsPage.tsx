688d160e962d001bb0128469f4d7fbd7e861d745
Batubo Victory <victorybatubo76@gmail.com>
2026-03-19 11:27:04 +0100

    feat: Import data from supabase DB

--- app/client/src/pages/listings/ListingsPage.tsx
+++ app/client/src/pages/listings/ListingsPage.tsx
@@ -2,11 +2,22 @@ import ListingCard from '@/components/features/cards/ListingCard'
 import ListingLoadingState from '../../components/ui/LoadingStates/ListingLoadingState'
 import SearchPanel from '../../components/features/SearchPanel'
 import PageLink from '../../components/ui/links/PageLink'
-import { useSelector } from 'react-redux'
-import { selectFilteredListings } from '@/state/listings/listingsSlice'
+import { useEffect, useState } from 'react'
+import type { Listing } from '@/types/Listing'
+import axios from 'axios'
 
 const ListingsPage = () => {
-    const filteredListings = useSelector(selectFilteredListings)
+    const [listings, setListings] = useState<Listing[]>([]);
+
+    useEffect(() => {
+        const fetchListings = async () => {
+            const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/api/listings`);
+
+            setListings(data)
+        }
+
+        fetchListings();
+    }, [])
 
   return (
     <section className='mt-42 flex flex-col gap-48 items-center'>
@@ -14,8 +25,8 @@ const ListingsPage = () => {
 
         <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full max-w-7xl mx-auto px-6 place-items-center py-16'>
             {
-                filteredListings.length !== 0 ? (
-                    filteredListings.map(listing => (
+                listings && listings.length !== 0 ? (
+                    listings.map(listing => (
                     <ListingCard 
                         key={listing.id}
                         id={listing.id}
