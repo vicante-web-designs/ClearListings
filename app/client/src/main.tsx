b5e1420dbbe2c5758c1893cc992aab19a8eee7e4
Batubo Victory <victorybatubo76@gmail.com>
2026-04-14 14:20:37 +0100

    fix: Correct signUpPage file name

--- app/client/src/main.tsx
+++ app/client/src/main.tsx
@@ -14,7 +14,7 @@ import AboutPage from './pages/AboutPage.tsx';
 import CreateListingPage from './pages/listings/CreateListingPage.tsx'
 import EditListing from './components/features/listingFeatures/EditListing.tsx'
 import App from './App.tsx'
-import SignUpPage from './pages/auth/SignUpPage'
+import SignUpPage from './pages/auth/SignUpPage.tsx'
 import LoginPage from './pages/auth/LoginPage.tsx'
 import AgentLoginPage from './pages/auth/AgentLoginPage.tsx'
 
