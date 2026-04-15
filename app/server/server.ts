7cbf228f7af9f1d969b893f58a70a87d730c9717
Batubo Victory <victorybatubo76@gmail.com>
2026-04-15 15:25:41 +0100

    refactor: Tidy up server.js

--- app/server/server.ts
+++ app/server/server.ts
@@ -1,13 +1,12 @@
+// Imports
 import dotenv from 'dotenv';
-dotenv.config()
-
+dotenv.config();
 import express from 'express';
 import cors from 'cors';
-
-import listingRouter from './src/routes/listingRouter.js'
+import listingRouter from './src/routes/listingRouter.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 const allowedOrigins = [
     'http://localhost:5173', //development origin
@@ -25,6 +24,7 @@ app.use(cors({
 }));
 app.use(express.json())
 
+//Routes
 app.use('/api/listings', listingRouter)
 
 app.listen(PORT, () => {
