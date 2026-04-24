2063f573a9357b1aa1d1e9ed984051185d57d8e9
Batubo Victory <victorybatubo76@gmail.com>
2026-04-24 13:56:04 +0100

    feat: Add listing controllers and create/delete endpoints

---
+++ app/server/src/controllers/listingControllers.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from 'express';
+import supabase from '../config/supabase.js';
+import type { Listing } from '../types/Listing.js';
+import parsePrice from '../utils/parsePriceFilter.js';
+
+// Get all listings
+export const getAllListings = async (req: Request, res: Response) => {
+  try {
+        const { title, city, state, location, status, minPrice, maxPrice, propertyType } = req.query as Record<string, string>;
+
+        let query = supabase.from('listings').select('*')
+
+        if (title) query = query.ilike('title', `%${title}%`)
+        if (city) query = query.ilike('city', `%${city}%`)
+        if (state) query = query.ilike('state', `%${state}%`)
+        if (location) query = query.ilike('location', `%${location}%`)
+        if (status) query = query.ilike('status', `%${status}%`)
+        if (propertyType) query = query.ilike('propertyType', `%${propertyType}%`)
+        if (minPrice) query = query.gte('price', parsePrice(minPrice))
+        if (maxPrice) query = query.lte('price', parsePrice(maxPrice))
+
+        const { data, error } = await query
+
+        if (error) throw error
+        res.json(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+// Get one Listing
+export const getOneListing = async (req: Request, res:Response) => {
+    const { id } = req.params
+
+    const { data, error } = await supabase.from('listings').select('*').eq('id', id).single()
+
+    if (error || !data){
+        return res.status(404).json({ error: 'Listing not found'})
+    }
+
+    res.json(data)
+}
+
+// Create Listing
+export async function createListing(req: Request, res: Response){
+
+    const listingData: Omit<Listing, 'id' | 'createdAt'> = req.body
+
+    const { data, error } = await supabase.from('listings').insert(listingData).select().single();
+
+    if (error) {
+        return res.status(500).json({ error: error.message })
+    }
+    
+    res.status(201).json(data)
+}
+
+// Update one listing
+// Create one listing
+// Delete one listing
--- app/server/src/routes/listingRouter.ts
+++ app/server/src/routes/listingRouter.ts
@@ -1,53 +1,15 @@
 import express from 'express';
-import { createListing, getOneListing } from '../services/listingService.js';
-import supabase from '../config/supabase.js';
-import parsePrice from '../utils/parsePriceFilter.js';
+import { createListing, getOneListing,getAllListings } from '../controllers/listingControllers.js';
 
 const router = express.Router()
 
 // Create listings
-router.post('/', async (req, res) => {
-    try {
-        const listing = await createListing(req.body);
-        res.json(listing)
-    } catch (error: any) {
-        res.status(500).json({ error: error.message })
-    }
-})
+router.post('/', createListing)
 
 // Get all listings + filtering
-router.get('/', async (req, res) => {
-    try {
-        const { title, city, state, location, status, minPrice, maxPrice, propertyType } = req.query as Record<string, string>;
-
-        let query = supabase.from('listings').select('*')
-
-        if (title) query = query.ilike('title', `%${title}%`)
-        if (city) query = query.ilike('city', `%${city}%`)
-        if (state) query = query.ilike('state', `%${state}%`)
-        if (location) query = query.ilike('location', `%${location}%`)
-        if (status) query = query.ilike('status', `%${status}%`)
-        if (propertyType) query = query.ilike('propertyType', `%${propertyType}%`)
-        if (minPrice) query = query.gte('price', parsePrice(minPrice))
-        if (maxPrice) query = query.lte('price', parsePrice(maxPrice))
-
-        const { data, error } = await query
-
-        if (error) throw error
-        res.json(data)
-    } catch (error: any) {
-        res.status(500).json({ error: error.message })
-    }
-})
+router.get('/', getAllListings)
 
 // Get one listing
-router.get('/:id', async (req, res) => {
-    try {
-        const listing = await getOneListing(req.params.id)
-        res.json(listing)
-    } catch (error: any) {
-        res.status(500).json({ error: error.message })
-    }
-})
+router.get('/:id', getOneListing)
 
 export default router;
