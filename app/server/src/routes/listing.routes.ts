16e6c91f88b076cfd73bf607b592e5eac87567e0
Batubo Victory <victorybatubo76@gmail.com>
2026-04-24 15:25:09 +0100

    feat: Add ownership check to listing route

--- app/server/src/routes/listing.routes.ts
+++ app/server/src/routes/listing.routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { createListing, getOneListing,getAllListings, updateListing, deleteOneListing } from '../controllers/listing.controller.js';
 import { authMiddleware } from '../middleware/auth.middleware.js';
 import { requireRole } from '../middleware/role.middleware.js';
+import { listingOwnerMiddleware } from '../middleware/listingOwner.middleware.js';
 
 
 
@@ -25,9 +26,9 @@ router.get('/:id', getOneListing)
 router.post('/', authMiddleware, requireRole('agent', 'admin'), createListing)
 
 // Update listing
-router.put('/:id', authMiddleware, requireRole('agent', 'admin'), updateListing)
+router.put('/:id', authMiddleware, listingOwnerMiddleware, requireRole('agent', 'admin'), updateListing)
 
 // Delete one listing
-router.delete('/:id', authMiddleware, requireRole('agent', 'admin'), deleteOneListing)
+router.delete('/:id', authMiddleware, requireRole('agent', 'admin'), listingOwnerMiddleware, deleteOneListing)
 
 export default router;
